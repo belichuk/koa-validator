cfb573b46bdb033e8da156ab00cf900c4c8a9779
Nikolay Belichuk <belichuk@gmail.com>
2018-03-26 22:57:34 +0300

    [Fix] Message vailidation sprintf like es6 template

--- validators/helpers/ValidationError.js
+++ validators/helpers/ValidationError.js
@@ -1,8 +1,11 @@
 class ValidationError {
-	constructor(message, _props) {
-		let props = Object.assign({}, _props);
-
-		this.message = message || 'Unspecified validation error';
+	constructor(message = '', _props) {
+		const props = Object.assign({}, _props);
+		const args = (Array.isArray(props.args) && props.args) || [];
+		
+		this.message = message.replace(/\$\{([0-9]+)\}/g, (str, val) => {
+			return (typeof args[val] !== 'undefined') ? args[val] : '';
+		});
 		
 		// copy from argument properties
 		for (let key in props) {
